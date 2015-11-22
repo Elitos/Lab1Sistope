1048d41cacf0a631c9b00bdd836e4dcd66ec6cf5
Elias <elias.gonzalezma@usach.cl>
2015-11-22 11:40:00 -0300

    while con corte en exit execv/pipe/dup

--- Practicas/pipesDupExec.c
+++ Practicas/pipesDupExec.c
@@ -6,11 +6,36 @@
 #include <sys/wait.h>
 #include <time.h>
 
-
-
+#define OK       0
+#define NO_INPUT 1
+#define TOO_LONG 2
 #define LEER 0
 #define ESCRIBIR 1
 
+static int getLine (char *prmpt, char *buff, size_t sz) {
+    int ch, extra;
+
+    // Get line with buffer overrun protection.
+    if (prmpt != NULL) {
+        printf ("%s", prmpt);
+        fflush (stdout);
+    }
+    if (fgets (buff, sz, stdin) == NULL)
+        return NO_INPUT;
+
+    // If it was too long, there'll be no newline. In that case, we flush
+    // to end of line so that excess doesn't affect the next call.
+    if (buff[strlen(buff)-1] != '\n') {
+        extra = 0;
+        while (((ch = getchar()) != '\n') && (ch != EOF))
+            extra = 1;
+        return (extra == 1) ? TOO_LONG : OK;
+    }
+
+    // Otherwise remove newline and give string back to caller.
+    buff[strlen(buff)-1] = '\0';
+    return OK;
+}
 
 int main(int argc, char *argv[]){
 
@@ -25,9 +50,9 @@ int main(int argc, char *argv[]){
   time_t t;
   int status;
 
-int k;
-
-  for(k = 0; k <= 1; k++){
+char comando[10] = "Co!";
+int comando_lengh = 0; 
+while(strcmp(comando,"exit") != 0){
       pipe(pipe1);
       printf("Escritura: %d, Lectura: %d\n",pipe1[LEER],pipe1[ESCRIBIR]);
       if ((pid = fork()) < 0)
@@ -38,7 +63,7 @@ int k;
         dup2(pipe1[ESCRIBIR],STDOUT_FILENO);
         close(pipe1[ESCRIBIR]);
         close(pipe1[LEER]);
-        execv("./otro", (char *[]){ "./otro", argv[k], NULL });;
+        execv("./otro", (char *[]){ "./otro", comando, NULL });;
         perror("execv() error");
         _exit(1);
       }
@@ -63,7 +88,11 @@ int k;
           else puts("child did not exit successfully");
         }
       } while (pid == 0);
-    }
+ 
+   while(getLine("",comando,sizeof(comando)) == NO_INPUT){}
+    // gets(comando);
+  
+} 
 
    return 0;
 }
